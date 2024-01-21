f0cb582de5f25fb3a613b17bbe50a3663f6b7882
Stéphane Bouchard <softwarequality@icloud.com>
2024-01-21 10:18:29 -0500

    21 janvier 10h18

---
+++ perl/types.pl
@@ -0,0 +1,32 @@
+# Déclaration de variable de type entier
+my $entier = 1;
+
+# Déclaration de variable de type float
+my $decimal = 3.14;
+
+# Déclaration de variable de type string
+my $chaine = "Hello, World!";
+print $chaine;
+
+# Déclaration de variable de type boolean
+my $booleen = true;
+
+# Déclaration de variable de type tableau
+my @tableau = ("Hello", "World", "!");
+print $tableau[0];
+
+# Déclaration de variable de type dictionnaire
+my %dictionnaire = ("clef1", "valeur1", "clef2", "valeur2");
+print $dictionnaire{"clef1"};
+
+# Déclaration de variable de type n-uplet
+my ($x, $y) = (1, 2);
+print $x;
+
+# Déclaration de variable de type n-uplet nommé
+my %point = ("x", 1, "y", 2);
+print $point{"x"};
+
+# Déclaration de variable de type n-uplet nommé
+my %point = ("x" => 1, "y" => 2);
+print $point{"x"};
