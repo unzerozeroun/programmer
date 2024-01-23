200b8492515ba197a3c52e106a9086b3f5dd2b8f
Stéphane Bouchard <softwarequality@icloud.com>
2024-01-23 00:21:48 -0500

    23 janvier 0h21

---
+++ c/syntaxe.c
@@ -0,0 +1,13 @@
+#include <stdio.h> // Cette ligne inclut le fichier d'en-tête standard d'entrée/sortie pour utiliser la fonction printf.
+
+/*
+Compilation :
+    gcc syntaxe.c -o syntaxe
+Exécution :
+    ./syntaxe
+*/
+
+int main() { // Définit la fonction principale main qui est le point d'entrée de chaque programme en C.
+    printf("Hello World!\n"); // Affiche le texte "Hello World!" suivi d'un retour à la ligne.
+    return 0; // Retourne 0, qui signifie que le programme s'est exécuté sans erreur.
+}
